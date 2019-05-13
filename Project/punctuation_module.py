2b6572586d6a697969cda4432948bf705fcc6f4d
ClaudiuIoanMaftei <claudiu.ioan.maftei@gmail.com>
2019-05-13 12:36:45 +0300

    Update punctuation_module.py

--- Project/punctuation_module.py
+++ Project/punctuation_module.py
@@ -1,10 +1,25 @@
-def compute_punctuation():
-    pass
-
+from tokens_and_lemmas_module import read
 
 def get_punctuation_score(sentence):
-    pass
+    streaks=0
+    total=0
+    prev_char=""
+    for char in sentence:
+        if char in ",.!?":
+            if char==prev_char: streaks+=1
+            total += 1
+        prev_char=char
 
+    if total!=0: return streaks/total
+    return 0
+
+def compute_punctuation():
+    data_msg, dex = read(file="Data/agr_en_train.csv")
+    data_msg = [data[0] for data in data_msg]
+    fd = open("Outputs/punctuations.csv", "w+")
+    for message_index in range(0, len(data_msg)):
+        score = get_punctuation_score(data_msg[message_index])
+        fd.write(str(message_index) + ',' +str(score) +  '\n')
 
 if __name__ == '__main__':
     compute_punctuation()
